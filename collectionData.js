bcd2737c2fcb9c500abbbd3d2ecc8b95277d124e
Neil <neilsolomon@gmail.com>
2014-04-17 19:56:02 -0400

    Started to do login stuff... unfinished

--- collectionData.js
+++ collectionData.js
@@ -2,7 +2,7 @@ var data = {
 	'object1':{
 	name: 'Object Number 1',
 	tag: 'weird'
-}
+	}
 };
 
 module.exports = data;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 //Express + Handlebars
 var express = require('express');
-var expressHbs = require('express3-handlebars');
+var expressHbs = require('express3-handlebars'); //Handlebars Module
 var handlebars = expressHbs.create({
 	defaultLayout: 'main'
+	//Main refers to my boilerplate/customized HTML in /layouts
 });
 var request = require('request');
 var app = express();
 
 app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars'); //Set the view engine to handlebars
 
 app.get('/', function(req, res){
 	res.render('index');
@@ -20,15 +21,23 @@ app.get('/page2', function(req, res){
 
 var collectionData = require('./collectionData');
 
-app.get('/collection/:objectName', function(req, res){
+app.get('/collection/:objectName', function(req, res){ //Reference to data comes in dynamically thru /:objectName via the URL
 		var objectName = req.params.objectName;
-		var data = collectionData[objectName];
-		res.render('collection', data);
+		var data = collectionData[objectName]; //refers to JavaScript object in collectionData.js
+		res.render('collection', data); //renders 'data' from collectionData.js
 	});
 
+app.get('/users/:user_id', function(req, res){
+	res.send("The user ID is: " + req.params.user_id);
 
+})
+
+app.get('/login', function(req,res){
+	res.render('login');
+
+})
 
 app.use('/public',
-		express.static('public'));
+	express.static('public'));
 
 app.listen(5000);
