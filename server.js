b42fd6645c2482943b4e1b6b083e41e1b4338452
Neil <neilsolomon@gmail.com>
2014-04-23 23:00:37 -0400

    Comments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+//Requirements
 var express = require('express');
-var app 	= express();
+var app = express();
 // var moment  = require('moment');
 var bodyParser = require('body-parser');
 var expressHbs = require('express3-handlebars');
@@ -7,24 +8,27 @@ var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 var MongoStore = require('connect-mongo')(expressSession);
 
-//Mongo Settings
+//mongoDB Database Settings
 var MongoClient = require('mongodb').MongoClient;
 var mongoURL = "mongodb://test:test@ds045557.mongolab.com:45557/135is";
 
-
+//Enable Cookies
 app.use( bodyParser() );
 app.use( cookieParser() );
+
+//Enable Sessions and Mongo-connected Sessions
 app.use( expressSession({
 	secret: 'thesecret',
 	store: new MongoStore({url: mongoURL})
 }) );
 
+//Enable Handlebars
 app.engine('handlebars', expressHbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
-//Checking for Session Cookie
+//Checking for Session Cookie, then move on to next part of server.js
 function checkLoggedIn(req, res, next){
-	console.log('inside checkLoggedIn');
+	console.log('inside checkLoggedIn function');
 
 	if (req.session.username) {
 		res.locals.loggedInUsername = req.session.username;
@@ -32,13 +36,14 @@ function checkLoggedIn(req, res, next){
 	next();
 }
 
-app.use( checkLoggedIn );
+app.use(checkLoggedIn);
 
+//Serve up index page at /
 app.get('/', function(req, res){
-
 	res.render('index');
 });
 
+//Playing with param query strings in URL
 app.get('/users/:user_id', function(req,res){
 	res.send("The user id is: " + req.params.user_id);
 });
@@ -52,10 +57,12 @@ app.get('/users/:user_id', function(req,res){
 // 	res.render("people", {id: id});
 // });
 
+//Login Page View
 app.get('/login', function(req, res){
 	res.render('login');
 });
 
+//Check password (hardcoded)
 function passwordIsValid(user, pass) {
 	if (user === 'itpclass' && pass === 'letmein') {
 		return true;
@@ -64,6 +71,7 @@ function passwordIsValid(user, pass) {
 	}
 }
 
+//Post Login User/Pass for session recognition
 app.post('/login', function(req, res){
 	console.log('body params:', req.body);
 
@@ -93,14 +101,17 @@ app.post('/login', function(req, res){
 // 	res.send("session.myValue: " + req.session.myValue);
 // });
 
+//Registry View
 app.get('/registry', function(req, res){
 	res.render('registry');
 });
 
+//Data Viz View
 app.get('/stats', function(req, res){
 	res.render('stats');
 });
 
+//About View
 app.get('/about', function(req, res){
 	res.render('about');
 });
@@ -109,37 +120,43 @@ app.use('/public', express.static('public'));
 
 var port = Number(process.env.PORT || 5000);
 
+//Define Global db variable to be able to access db outside of MongoClient.connect
+var db;
+
 //Playing with mongo and URL query strings
 //Normally use app.post via req.body
-var db;
 // database?username=abcd&status=hello
 app.get('/database', function(req, res){
-		var username = req.query.username
-		var status = req.query.status;
-		var collection = db.collection('test_insert');
-		collection.insert({username:username, status:status}, function(err, count){
-				if (err){
-					console.log('Got an error!', err);
-				}
+	var username = req.query.username
+	var status = req.query.status;
+	var collection = db.collection('test_insert');
+	collection.insert({username:username, status:status}, function(err, count){
+		if (err){
+			console.log('Got an error!', err);
+		}
 
-				res.send('The count is:' + count);
-		});
+		res.send('The count is:' + count);
+	});
 });
 
+
+//best way to display/sort database
 app.get('/read_database', function(req,res){
-		var collection = db.collection('test_insert');
-		collection.find({username:'neil'}).toArray(function(err, items){
-			var item = items[0];
-			res.send('The user is: ' + item.status);
-		});
+	var collection = db.collection('test_insert');
+	collection.find({username:'neil'}).toArray(function(err, items){
+		var item = items[0];
+		res.send('The user is: ' + item.status);
+	});
 });
 
+//Connect to mongoDB
+//Must ensure db connection prior to running app (listening to port)
+//Set _db = db to be able to access database globally
 MongoClient.connect(mongoURL, function(err, _db){
 	if (err) {
 		console.log('There was an error: ' + err);
 	}
 	db = _db;
 	console.log('Connected to Mongo');
-
 	app.listen(port);
 });
